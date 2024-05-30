4cf4395dfac2283ca4639b37f9a0681f3491c0cc
Kei-K23 <keiksl2301@gmail.com>
2024-05-30 20:44:15 +0630

    Add home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: "./app.component.css"
 })
+
 export class AppComponent {
   title = 'angular-pj-1';
 }
