fd0f3ed9911d94768c22c821bcfd31ce5cc2f14e
Kei-K23 <keiksl2301@gmail.com>
2024-05-30 23:23:57 +0630

    Add housing-location component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HousingLocationComponent],
   template: `
     <section class="flex justify-center items-center w-full my-6">
       <form>
         <input class="px-4 py-1 border-blue-500 border-2 rounded-lg" type="text" placeholder="Filter by city" />
         <button class="ml-3 bg-blue-500 hover:bg-blue-600 transition active:scale-95 text-white px-2 py-1 rounded-md" type="button">Search</button>
       </form>
+    </section>
+    <section>
+      <app-housing-location></app-housing-location>
     </section>
   `,
   styleUrl: './home.component.css'
---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  template: `
+  <div>This is housing</div>
+  `,
+  styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+
+}
